c3319419420f20d29f0d71d5ef80032c894ac73f
Ryland Upson <rylandupson@outlook.com>
2021-08-04 09:09:15 -0500

    IdentitySamples

---
+++ EnrollmentApp.UI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace EnrollmentApp.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
