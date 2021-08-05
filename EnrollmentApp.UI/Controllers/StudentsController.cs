8975b9671a22fb8784959d494fb9c6b9295d94a8
Ryland Upson <rylandupson@outlook.com>
2021-08-05 15:19:09 -0500

    implemented file upload (kinda)

--- EnrollmentApp.UI/Controllers/StudentsController.cs
+++ EnrollmentApp.UI/Controllers/StudentsController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using EnrollmentApp.DATA.EF;
+using EnrollmentApp.UI.Utilities;
 
 namespace EnrollmentApp.UI.Controllers
 {
@@ -51,10 +53,42 @@ namespace EnrollmentApp.UI.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "StudentId,FirstName,LastName,Major,Address,City,State,Zip,Phone,Email,PhotoURL,SSID")] Student student)
+        public ActionResult Create([Bind(Include = "StudentId,FirstName,LastName,Major,Address,City,State,Zip,Phone,Email,PhotoURL,SSID")] Student student, HttpPostedFileBase photo)
         {
             if (ModelState.IsValid)
             {
+                #region File Upload
+                string file = "default.png";
+
+                if (photo != null)
+                {
+                    file = photo.FileName;
+                    string ext = file.Substring(file.LastIndexOf('.'));
+
+                    string[] goodExts = { ".jpeg", ".jfif", ".jpg", ".png", ".gif" };
+
+                    //Check that the uploaded file is in our list of acceptable exts & file sizw <= 4mb max from ASP.NET
+                    if (goodExts.Contains(ext.ToLower()) && photo.ContentLength <= 4194304)
+                    {
+                        //Create a new file name (using a GUID)
+                        file = Guid.NewGuid() + ext;
+
+                        #region Resize Image
+                        string savePath = Server.MapPath("~/Content/images/StudentImages/");
+
+                        Image convertedImage = Image.FromStream(photo.InputStream);
+
+                        int maxImageSize = 500;
+
+                        int maxThumbSize = 100;
+
+                        ImageUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+                        #endregion
+                    }
+                    student.PhotoURL = file;
+                }
+                #endregion
+
                 db.Students.Add(student);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -86,10 +120,53 @@ namespace EnrollmentApp.UI.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "StudentId,FirstName,LastName,Major,Address,City,State,Zip,Phone,Email,PhotoURL,SSID")] Student student)
+        public ActionResult Edit([Bind(Include = "StudentId,FirstName,LastName,Major,Address,City,State,Zip,Phone,Email,PhotoURL,SSID")] Student student, HttpPostedFileBase photo)
         {
             if (ModelState.IsValid)
             {
+                #region File Upload
+                //Check to see if a new file has been uploaded. If not the HiddenFor() in the view will maintain the original value
+                string file = "default.png";
+
+                if (photo != null)
+                {
+                    //get the name
+                    file = photo.FileName;
+
+                    //capture the extension
+                    string ext = file.Substring(file.LastIndexOf('.'));
+
+                    //Create a "whitelisted" array of acceptable exts
+                    string[] goodExts = { ".jpeg", ".jpg", ".png", ".gif" };
+
+                    //Check the ext and file size
+                    if (goodExts.Contains(ext.ToLower()) && photo.ContentLength < 4194304)
+                    {
+                        //Create a new file name
+                        file = Guid.NewGuid() + ext;
+
+                        //Resize the image
+                        string savePath = Server.MapPath("~/Content/images/StudentImages/");
+
+                        Image convertedImage = Image.FromStream(photo.InputStream);
+
+                        int maxImageSize = 500;
+
+                        int maxThumbSize = 100;
+
+                        ImageUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+
+                        if (student.PhotoURL != null && student.PhotoURL != "default.png")
+                        {
+                            string path = Server.MapPath("~/Content/images/StudentImages/");
+                            ImageUtility.Delete(path, student.PhotoURL);
+                        }
+                        //update the property of the book object
+                        student.PhotoURL = file;
+                    }
+                }
+                #endregion
+
                 db.Entry(student).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -120,6 +197,10 @@ namespace EnrollmentApp.UI.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             Student student = db.Students.Find(id);
+
+            string path = Server.MapPath("~/Content/images/StudentImages/");
+            ImageUtility.Delete(path, student.PhotoURL);
+
             db.Students.Remove(student);
             db.SaveChanges();
             return RedirectToAction("Index");
